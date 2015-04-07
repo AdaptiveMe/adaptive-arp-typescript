da1cc3d7e6e7a853166bb5ed526829c240945899
Carlos Lozano <carlos@adaptive.me>
2015-04-07 23:05:19 +0200

    Update postcompile.sh

--- adaptive-arp-ts/postcompile.sh
+++ adaptive-arp-ts/postcompile.sh
@@ -6,8 +6,4 @@ rm $HOME_DIR/src_units/*.js
 rm $HOME_DIR/src_units/*.js.map
 rm $HOME_DIR/src_units/*.d.ts
 
-mv $HOME_DIR/Adaptive.js $HOME_DIR/adaptive.js
-mv $HOME_DIR/Adaptive.js.map $HOME_DIR/adaptive.js.map
-mv $HOME_DIR/Adaptive.d.ts $HOME_DIR/adaptive.d.ts
-
 exit 0
