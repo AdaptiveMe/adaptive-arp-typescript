7215ec36533d72976f5dd63f4829faf6f0253d13
carloslozano <carlos@adaptive.me>
2015-04-07 17:11:38 +0000

    Release from API 2.2.6 at Tue Apr  7 17:09:21 UTC 2015.

--- adaptive-arp-ts/src_units/BaseCallback.ts
+++ adaptive-arp-ts/src_units/BaseCallback.ts
@@ -27,7 +27,7 @@ Contributors:
 
 Release:
 
-    * @version v2.2.5
+    * @version v2.2.6
 
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
@@ -95,7 +95,7 @@ module Adaptive {
              The version of the API.
           */
           getAPIVersion() : string {
-               return "v2.2.5"
+               return "v2.2.6"
           }
 
      }
