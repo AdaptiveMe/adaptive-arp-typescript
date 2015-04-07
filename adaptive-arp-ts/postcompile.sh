dbd1d3a2600f199513d62aaa58a98d8a9f6c0fa1
Carlos Lozano <carlos@adaptive.me>
2015-04-07 23:07:40 +0200

    Update postcompile.sh

--- adaptive-arp-ts/postcompile.sh
+++ adaptive-arp-ts/postcompile.sh
@@ -2,8 +2,8 @@
 echo "Removing redundant files."
 HOME_DIR=`pwd`
 
-rm $HOME_DIR/src_units/*.js
-rm $HOME_DIR/src_units/*.js.map
-rm $HOME_DIR/src_units/*.d.ts
+rm -f $HOME_DIR/src_units/*.js
+rm -f $HOME_DIR/src_units/*.js.map
+rm -f $HOME_DIR/src_units/*.d.ts
 
 exit 0
