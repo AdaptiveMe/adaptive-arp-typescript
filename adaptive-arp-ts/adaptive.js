2509180f63e90e4d9157c74ecb5e4ab905ddc38b
carloslozano <carlos@adaptive.me>
2015-04-08 08:16:59 +0000

    API Specification 2.2.8 at Wed Apr  8 08:16:45 UTC 2015.

--- adaptive-arp-ts/adaptive.js
+++ adaptive-arp-ts/adaptive.js
@@ -33,7 +33,7 @@ Contributors:
 
 Release:
 
-    * @version v2.2.7
+    * @version v2.2.8
 
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
@@ -56,7 +56,7 @@ var Adaptive;
        @property {string} bridgeApiVersion
        The Adaptive Runtime Platform API specification version.
     */
-    Adaptive.bridgeApiVersion = "v2.2.7";
+    Adaptive.bridgeApiVersion = "v2.2.8";
     /**
        @private
        @class Adaptive.Dictionary
