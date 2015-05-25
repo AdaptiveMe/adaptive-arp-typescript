a6d593ac00dcd2d0d67c709faa18bdcc165c605b
carloslozano <carlos@adaptive.me>
2015-05-25 08:54:23 +0000

    Release from API 2.2.13 at Mon May 25 08:52:39 UTC 2015.

--- adaptive-arp-ts/src_units/CommonUtil.ts
+++ adaptive-arp-ts/src_units/CommonUtil.ts
@@ -27,7 +27,7 @@ Contributors:
 
 Release:
 
-    * @version v2.2.12
+    * @version v2.2.13
 
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
@@ -61,7 +61,7 @@ module Adaptive {
         @property {string} bridgeApiVersion
         The Adaptive Runtime Platform API specification version.
      */
-     export var bridgeApiVersion : string = "v2.2.12";
+     export var bridgeApiVersion : string = "v2.2.13";
 
      /**
         @class Adaptive.IDictionary
